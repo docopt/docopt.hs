a6b8e14d33f186a5f5c629c4be1f38728faa6bd1
Ryan Artecona <ryanartecona@gmail.com>
2015-02-27 10:57:46 -0500

    add `parseUsage` and `parseUsageOrExit`

--- System/Console/Docopt/NoTH.hs
+++ System/Console/Docopt/NoTH.hs
@@ -8,6 +8,7 @@ module System.Console.Docopt.NoTH
 import Data.Map as M hiding (null)
 import System.Exit
 
+import System.Console.Docopt.Types
 import System.Console.Docopt.Public
 import System.Console.Docopt.ParseUtils
 import System.Console.Docopt.UsageParse (pDocopt)
@@ -17,6 +18,18 @@ import System.Console.Docopt.OptParse (getArguments)
 -- * Public API
 
 -- ** Main option parsing entry points
+
+parseUsage :: String -> Either ParseError Docopt
+parseUsage usg =
+  case runParser pDocopt M.empty "Usage" usg of
+    Left e       -> Left e
+    Right optfmt -> Right (Docopt optfmt usg)
+
+parseUsageOrExit :: String -> IO Docopt
+parseUsageOrExit usg = exitUnless $ parseUsage usg
+  where
+    exit message = putStrLn message >> exitFailure
+    exitUnless = either (const $ exit usg) return
 
 optionsWithUsage :: String -> [String] -> IO Arguments
 optionsWithUsage usage rawArgs =

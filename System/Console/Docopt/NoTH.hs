2c39fc9050cc8476b15ca94c9cc31ef1c44e68f5
Ryan Artecona <ryanartecona@gmail.com>
2015-02-28 19:22:11 -0500

    remove `optionsWithUsage[Debug]`

--- System/Console/Docopt/NoTH.hs
+++ System/Console/Docopt/NoTH.hs
@@ -1,7 +1,10 @@
 module System.Console.Docopt.NoTH
   (
-    module System.Console.Docopt.NoTH,
-    module System.Console.Docopt.Public
+    -- * Public API
+      parseUsage
+    , parseUsageOrExit
+
+    , module System.Console.Docopt.Public
   )
   where
 
@@ -12,12 +15,7 @@ import System.Console.Docopt.Types
 import System.Console.Docopt.Public
 import System.Console.Docopt.ParseUtils
 import System.Console.Docopt.UsageParse (pDocopt)
-import System.Console.Docopt.OptParse (getArguments)
 
-
--- * Public API
-
--- ** Main option parsing entry points
 
 parseUsage :: String -> Either ParseError Docopt
 parseUsage usg =
@@ -30,21 +28,3 @@ parseUsageOrExit usg = exitUnless $ parseUsage usg
   where
     exit message = putStrLn message >> exitFailure
     exitUnless = either (const $ exit usg) return
-
-optionsWithUsage :: String -> [String] -> IO Arguments
-optionsWithUsage usage rawArgs =
-    case runParser pDocopt M.empty "Usage" usage of
-        Left _    -> do putStrLn usage
-                        exitFailure
-        Right fmt -> case getArguments fmt rawArgs of
-            Left _           -> do putStrLn usage
-                                   exitFailure
-            Right parsedArgs -> return parsedArgs
-
-optionsWithUsageDebug :: String -> [String] -> IO Arguments
-optionsWithUsageDebug usage rawArgs =
-    case runParser pDocopt M.empty "Usage" usage of
-        Left err  -> fail $ show err
-        Right fmt -> case getArguments fmt rawArgs of
-            Left err         -> fail $ show err
-            Right parsedArgs -> return parsedArgs

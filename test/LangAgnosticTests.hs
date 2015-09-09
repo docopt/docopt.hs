b4c83c4cc9fbc76e8a418fe7da63b266aae113d3
Ryan Artecona <ryanartecona@gmail.com>
2015-09-09 09:32:58 -0400

    Add blacklist for expected-failing tests

--- test/LangAgnosticTests.hs
+++ test/LangAgnosticTests.hs
@@ -49,14 +49,68 @@ magenta = coloredString Magenta
 main :: IO ()
 main = do
   f <- readFile "test/testcases.docopt"
-  tests <- testsFromDocoptSpecFile "testcases.docopt" f
+  tests <- testsFromDocoptSpecFile "testcases.docopt" f blacklist
   counts <- runTestTT $ TestList tests
   exitWith $ if failures counts > 0
                 then ExitFailure 1
                 else ExitSuccess
 
-testsFromDocoptSpecFile :: String -> String -> IO [Test]
-testsFromDocoptSpecFile name testFile =
+blacklist :: (Int, Int) -> Bool
+-- Short/long option synonym equality (will fix)
+blacklist (4, 1) = True
+blacklist (4, 3) = True
+blacklist (7, 1) = True
+blacklist (8, 1) = True
+blacklist (8, 2) = True
+blacklist (64, 1) = True
+-- Partial-option disambiguation
+blacklist (4, 2) = True
+blacklist (6, 3) = True
+blacklist (6, 4) = True
+blacklist (12, 4) = True
+-- Stacked short options/flags disambiguation
+blacklist (14, 1) = True
+blacklist (70, 1) = True
+-- Option order insensitivity
+blacklist (15, 2) = True
+blacklist (16, 2) = True
+blacklist (17, 2) = True
+blacklist (18, 2) = True
+-- Argument lookup key ("<arg>" v. "arg"; should fix)
+blacklist (21, 1) = True
+blacklist (22, 1) = True
+blacklist (22, 3) = True
+blacklist (23, 1) = True
+blacklist (24, 1) = True
+blacklist (24, 2) = True
+blacklist (25, 2) = True
+blacklist (25, 3) = True
+blacklist (26, 1) = True
+blacklist (26, 2) = True
+blacklist (27, 1) = True
+blacklist (27, 2) = True
+blacklist (27, 3) = True
+blacklist (28, 1) = True
+blacklist (28, 3) = True
+blacklist (35, 1) = True
+blacklist (60, 1) = True
+blacklist (60, 2) = True
+blacklist (61, 1) = True
+blacklist (66, 1) = True
+blacklist (72, 1) = True
+-- Weirdly broken (argument capture; should fix)
+blacklist (33, 2) = True
+blacklist (33, 3) = True
+blacklist (34, 3) = True
+-- [options] expansion pruning (should fix)
+blacklist (67, 1) = True
+blacklist _ = False
+
+testsFromDocoptSpecFile :: String
+                        -> String
+                        -> ((Int, Int) -> Bool)
+                        -> IO [Test]
+testsFromDocoptSpecFile name testFile ignore =
   let notCommentLine x = null x || ('#' /= head x)
       testFileClean = unlines $ filter notCommentLine $ lines testFile
       caseGroups = filter (not . null) $ splitOn "r\"\"\"" testFileClean
@@ -91,9 +145,13 @@ testsFromDocoptSpecFile name testFile =
             Right a -> (a, "")
 
       let parsedArgsJSON = toJSON parsedArgs
-          testCaseSuccess = if rawTarget == "\"user-error\""
+          testCaseEquality = if rawTarget == "\"user-error\""
             then M.null parsedArgs
             else maybeTargetJSON == Just parsedArgsJSON
+          blacklisted = blacklist (icg, itc)
+          testCaseSuccess = if blacklisted
+            then not testCaseEquality
+            else testCaseEquality
 
       let testDescLines = [
               "Cmd: " ++ yellow cmdline,
@@ -103,6 +161,7 @@ testsFromDocoptSpecFile name testFile =
       let testMsg = unlines . filter (not . null) $
                       groupDescLines
                       ++ testDescLines
+                      ++ ["(Blacklisted)" | blacklisted]
                       ++ ["Failure: " ++ red (BS.unpack $ encode parsedArgsJSON)]
 
       let ti = TestCase $ testCaseSuccess @? testMsg
